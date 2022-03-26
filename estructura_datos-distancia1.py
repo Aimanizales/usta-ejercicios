524527f09a54b0a2c5e200a2e6aa177e8423e1e3
Aiman <aimanizales@gmail.com>
2022-03-26 14:51:03 -0500

    Exerciseupdated

--- estructura_datos-distancia1.py
+++ estructura_datos-distancia1.py
@@ -1,13 +1,30 @@
+from random import random
+
 cities = ['Paris', 'Amsterdam', 'Berlin', 'Prague', 'Vienna']
 ticketsPerCity = {}
-idGenerator = 0000
-MAX_UNITS = 20
+MAX_UNITS_PER_CITY = 20
 
+# 1. Primera función que se ejecuta:
 def init():
     initTicketsPerCity()
     selectOption()
 
+# 2. Esta función crea una lista vacía para cada 
+# ciudad de acuerdo a la variable cities:
+def initTicketsPerCity():
+    print(f'Hay {len(cities)} ciudades para este viaje.')
+    print(f'Cada ciudad puede tener hasta {MAX_UNITS_PER_CITY} tickets.')
+    cityCounter = 0
+    for city in cities:
+        ticketsPerCity[city]=[]
+        cityCounter += 1
+    showCitiesInfo(True)
+
+# Esta función es transversal a la aplicación y solo es para mostrar la
+# información actualizada de las ciudades y su número de tickets:
 def showCitiesInfo(hideNumbers = False):
+    if hideNumbers == True:
+        print('\n\CIUDADES:')
     count = 1
     for city in cities:
         numTickets = len(ticketsPerCity[city])
@@ -20,14 +37,7 @@ def showCitiesInfo(hideNumbers = False):
         print(f' - {counter}{city} -- [{message}]')
         count += 1
 
-def initInsertInfo():
-    print('\n¿A qué ciudad desea insertar el tiquete?:')
-    showCitiesInfo()
-
-    selectedCity = selectCity()
-    print(f'Ciudad seleccionada: {cities[selectedCity]}\n')
-    insertTicketInfo(selectedCity)
-
+# Permite únicamente seleccionar la ciudad digitando un número:
 def selectCity():
     selectedCity = int(input('\n>> Ingrese el número de la ciudad: ')) - 1
     if selectedCity >= len(cities) or selectedCity < 0:
@@ -36,27 +46,31 @@ def selectCity():
     else:
         return selectedCity
 
-# 
-def initTicketsPerCity():
-    print(f'Hay {len(cities)} ciudades para este viaje.')
-    cityCounter = 0
-    for city in cities:
-        ticketsPerCity[city]=[]
-        cityCounter += 1
-    showCitiesInfo(True)
+# Inicia el proceso de escoger la ciudad para insertar el nuevo ticket:
+def initInsertInfo():
+    print('\n¿A qué ciudad desea insertar el tiquete?:')
+    showCitiesInfo()
+
+    selectedCity = selectCity()
+    print(f'Ciudad seleccionada: {cities[selectedCity]}\n')
+    insertTicketInfo(selectedCity)
+
 
 def insertTicketInfo(selectedCity):
-    city = input('>> Ingrese la ciudad de origen: ')
-    startDate = input('>> Ingrese la fecha de inicio (dd/mm/aaaa): ')
+    print(f'Ingrese información del ticket:')
+    city = input('>> Ciudad de origen: ')
+    startDate = input('>> Fecha de inicio (dd/mm/aaaa): ')
 
-    ticket = dict(ticketId=idGenerator + 1, city=city, startDate=startDate)
+    id += 1
+    reference = random()
+    ticket = dict(ticketId=id, reference=reference, city=city, startDate=startDate)
 
     insertTicketIntoCityList(ticket, selectedCity)
 
 def insertTicketIntoCityList(ticket, selectedCity):
     currentCity = cities[selectedCity]
     ticketsPerCity[currentCity].append(ticket)
-    print(f'Ticket ingresado a la ciudad de {currentCity}\n')
+    print(f'Ingresado ticket para {currentCity}!!!\n')
     print(ticketsPerCity)
     selectOption()
 
@@ -65,27 +79,36 @@ def initDeleteTicket():
     showCitiesInfo()
 
     selectedCity = selectCity()
-    print(f'Ciudad seleccionada: {cities[selectedCity]}\n')
+    print(f'\nCiudad seleccionada: {cities[selectedCity]}\n')
     deleteTicketInfo(selectedCity)
 
 def deleteTicketInfo(selectedCity):
-    if len(ticketsPerCity[selectedCity]) > 0:
-        print(f'\nSe borrará el último ticket ingresado a la ciudad de {cities[selectedCity]}')
+    currentCity = cities[selectedCity]
+    currentCityTickets = ticketsPerCity[currentCity]
+
+    if len(currentCityTickets) == 0:
+        print(f'\n{cities[selectedCity]} no posee tickets ingresados aún.')
     else:
-        print(f'{cities[selectedCity]} no posee tickets ingresados aún.')
-        selectOption()
+        currentCityTickets.pop()
+        print(f'\nEliminado último ticket de {cities[selectedCity]}')
+        showCitiesInfo(True)
+    
+    selectOption()
 
 def selectOption():
-    print('\nSeleccione una de las opciones:')
-    print('  1. Insertar un ticket')
-    print('  2. Borrar un ticket')
-    selectedOption = int(input('\n>> Ingrese una opción: '))
-    if selectedOption < 1 or selectedOption > 2:
-        print('Seleccione correctamente una opción')
+    print('\n1: Insertar ticket')
+    print('2: Borrar ticket')
+    print('3: Ver lista de tickets')
+    selectedOption = int(input('\n>>>>> Ingrese una opción: '))
+    if selectedOption < 1 or selectedOption > 3:
+        print('Seleccione correctamente una opción.')
         selectOption()
     elif selectedOption == 1:
         initInsertInfo()
     elif selectedOption == 2:
         initDeleteTicket()
+    else:
+        showCitiesInfo(True)
+        selectOption()
 
 init()
