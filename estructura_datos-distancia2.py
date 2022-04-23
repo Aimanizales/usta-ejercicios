3e74535a941729b5e20932eab13190c32c572924
Aiman <aimanizales@gmail.com>
2022-04-23 12:33:13 -0500

    Modified eval distancia 2

--- estructura_datos-distancia2.py
+++ estructura_datos-distancia2.py
@@ -15,24 +15,23 @@ def init():
 def initTicketsPerCity():
     for city in cities:
         ticketsPerCity[city] = []
-    showCitiesInfo(True)
 
 def printTicketList():
-    print('Tickets por ciudad de origen:')
+    print('\nLISTA DE TICKETS COMPRADOS POR CIUDAD:')
     for key, value in ticketsPerCity.items():
         print(f'- {key}:')
         m = 1
         for ticket in value:
             if ticket:
-                print(f' {m}. {ticket}')
+                print(f'  {m}. {ticket}')
                 m += 1
             else:
                 print('[]')
 
-    print(f'\nTickets a Viena ({len(ticketsToVienna)}):')
+    print(f'\nLISTA DE TICKETS A VIENNA ({len(ticketsToVienna)}):')
     n = 1
     for ticket in ticketsToVienna:
-        print(f' {n}. {ticket}')
+        print(f'   {n}. {ticket}')
         n += 1
 
 # Esta función es transversal a la aplicación y solo es para mostrar la
@@ -41,7 +40,7 @@ def printTicketList():
 # una usando el parámetro hideNumbers:
 def showCitiesInfo(hideNumbers = False):
     if hideNumbers == True:
-        print('\nCantidad de tickets por ciudad de origen:')
+        print('\nCANTIDAD DE TICKETS COMPRADOS POR CIUDAD:')
     count = 1
     message = ''
     for city in cities:
@@ -52,7 +51,7 @@ def showCitiesInfo(hideNumbers = False):
             if numTickets != 1:
                 message += 's'
             counter = ''
-        print(f' - {counter}{city} {message}')
+        print(f' {counter}{city}: {message}')
         count += 1
 
 # Permite únicamente seleccionar la ciudad digitando un número:
@@ -66,7 +65,7 @@ def selectCity():
 
 # Inicia el proceso de escoger la ciudad para insertar el nuevo ticket:
 def initInsertInfo():
-    print('\nDigite el número de la ciudad de origen:')
+    print('\nCiudades de origen:')
     showCitiesInfo()
 
     selectedCity = selectCity()
@@ -79,7 +78,7 @@ def initInsertInfo():
 def insertTicketInfo(selectedCity):
     global numTicketSequence
     numTicketSequence += 1
-    print(f'Ingrese fecha de partida (dd/mm/aaaa):')
+    print(f'Ingrese día y mes de partida (dd/mm):')
 
     # Datos del ticket ingresados por el usuario:
     departureDate = input('>> ')
@@ -87,9 +86,9 @@ def insertTicketInfo(selectedCity):
     # Creación del ticket:
     ticket = dict(
         ticket_id = random.randint(0, 100000),
-        reference_number = numTicketSequence,
+        ref_number = '000' + str(numTicketSequence),
         origin_city = cities[selectedCity],
-        departure_date = departureDate
+        departure_date = departureDate + '/2022'
     )
 
     insertTicketIntoCityList(ticket, selectedCity)
@@ -107,36 +106,25 @@ def insertTicketIntoCityList(ticket, selectedCity):
     printTicketList()
     selectOption()
 
-# Selecciona la ciudad a borrar el ticket:
-def initDeleteTicket():
-    print('\nDigite el número de la ciudad de origen:')
-    showCitiesInfo()
+def deleteViennaTicketFIFO():
 
-    selectedCity = selectCity()
-    print(f'\nCiudad seleccionada: {cities[selectedCity]}\n')
-    deleteTicketInfo(selectedCity)
-
-# Borra el ticket de la ciudad elegida. Valida si la ciudad no tiene: 
-def deleteTicketInfo(selectedCity):
-    currentCity = cities[selectedCity]
-    currentCityTickets = ticketsPerCity[currentCity]
-
-    if len(currentCityTickets) == 0:
-        print(f'\n{cities[selectedCity]} no posee tickets ingresados aún.')
+    if len(ticketsToVienna) == 0:
+        print('\nLa lista Viena está vacía.')
     else:
-        currentCityTickets.pop()
-        print(f'\nEliminado último ticket de {cities[selectedCity]}')
-        showCitiesInfo(True)
+        ticketsToVienna.pop(0)
+        print(f'\nEliminado primer ticket de la lista Viena')
     
     selectOption()
 
 # El controlador principal de la aplicación:
 def selectOption():
     print('\n'
-        '1: Insertar\n'
-        '2: Borrar\n'
-        '3: Mostrar lista\n'
-        '4: Salir'
+        '========================================\n'
+        '1: Insertar ticket\n'
+        '2: Borrar primer ticket de Viena (FIFO)\n'
+        '3: Ver cantidad de tickets comprados por ciudad\n'
+        '4: Ver lista completa de tickets\n'
+        '5: Salir'
     )
     selectedOption = input('\n>>>>> Ingrese una opción: ')
     if selectedOption.isdigit():
@@ -147,9 +135,12 @@ def selectOption():
         elif selectedOption == 1:
             initInsertInfo()
         elif selectedOption == 2:
-            initDeleteTicket()
+            deleteViennaTicketFIFO()
         elif selectedOption == 3:
             showCitiesInfo(True)
+            selectOption()
+        elif selectedOption == 4:
+            printTicketList()
             selectOption()
         else:
             quit()
