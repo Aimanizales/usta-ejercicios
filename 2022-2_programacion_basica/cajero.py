ea08ccd596f66ee5db2f45c9043d9ead039f1348
Aiman Samad <aiman.samad@zemoga.com>
2022-09-23 20:48:45 -0500

    task: Cajero WIP

--- 2022-2_programacion_basica/cajero.py
+++ 2022-2_programacion_basica/cajero.py
@@ -1,5 +1,25 @@
 import random
 
+bankNotesQuantities = {
+    10000: 100,
+    20000: 50,
+    50000: 20,
+    100000: 10
+}
+
+def printSelectedValue(amount):
+    valueWithDecimals = '{0:,}'.format(amount)
+    print(
+        '------------------------------------\n'
+        f'Valor digitado: ${valueWithDecimals}\n'
+        '------------------------------------\n'
+    )
+
+def calculateBankNotesToWithdraw(amount):
+    bankNotesQuantities[10000] -= 1
+    print(bankNotesQuantities[10000])
+    selectOption()
+
 def calculateSavings():
     # saldo: mayor a 100,000 y menor a 300,000.
     print('\n'
@@ -9,19 +29,22 @@ def calculateSavings():
     )
 
 def selectOption():
-    calculateSavings()
-    valueToWidtraw = input('\n>>>>> Digite el monto a retirar (mínimo $10,000):')
+    valueToWidtraw = input('\n>>>>> Digite el monto a retirar (mínimo $10,000, máximo: $600,000):')
     if valueToWidtraw.isdigit():
         valueToWidtraw = int(valueToWidtraw)
+        printSelectedValue(valueToWidtraw)
         if valueToWidtraw < 10000:
             print('El monto debe ser igual o mayor a $10,000')
+            selectOption()
+        if valueToWidtraw > 600000:
+            print('El monto debe ser igual o menor a $600,000')
             selectOption()
         if valueToWidtraw % 10000 != 0:
             print('El valor debe ser en unidades de 10,000')
             selectOption()
         else:
-            calculateSavings()
-            quit()
+            calculateBankNotesToWithdraw(valueToWidtraw)
+            # quit()
     else:
         print('Debe digitar solo números.')
         selectOption()
