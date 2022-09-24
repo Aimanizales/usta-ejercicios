6e54b491e4eb2852990975bbcfe967e3262112f3
Aiman Samad <aiman.samad@zemoga.com>
2022-09-23 19:46:49 -0500

    task: Cajero WIP

--- 2022-2_programacion_basica/README.md
+++ 2022-2_programacion_basica/README.md
@@ -1,4 +1,14 @@
 # Programación básica
 
 ## Evaluación distancia 1
-- Ejercicio cajero automático
+**Simulación de un cajero automático**
+Denominaciones:
+- 100 billetes de $10,000 ($1,000,000),
+- 50 billetes de $20,000 ($1,000,000),
+- 20 billetes de $50,000 ($1,000,000),
+- 10 billetes de $100,000 ($1,000,000).
+
+**Regla:**
+- Retiro mínimo: $10.000.
+- Saldo: aleatorio entre $100.000 y $300.000.
+- El cajero puede entregar dinero de diferente denominación.
--- 2022-2_programacion_basica/cajero.py
+++ 2022-2_programacion_basica/cajero.py
@@ -1,1 +1,33 @@
-# Ejercicio Eval. Distancia 1
+import random
+
+def calculateSavings():
+    # saldo: mayor a 100,000 y menor a 300,000.
+    print('\n'
+        '========================================\n'
+        f'Su saldo es de ${100000 + random.randint(1, 20) * 10000}\n'
+        'Gracias.'
+    )
+
+def selectOption():
+    calculateSavings()
+    valueToWidtraw = input('\n>>>>> Digite el monto a retirar (mínimo $10,000):')
+    if valueToWidtraw.isdigit():
+        valueToWidtraw = int(valueToWidtraw)
+        if valueToWidtraw < 10000:
+            print('El monto debe ser igual o mayor a $10,000')
+            selectOption()
+        if valueToWidtraw % 10000 != 0:
+            print('El valor debe ser en unidades de 10,000')
+            selectOption()
+        else:
+            calculateSavings()
+            quit()
+    else:
+        print('Debe digitar solo números.')
+        selectOption()
+
+# 1. Primera función que se ejecuta:
+def init():
+    selectOption()
+
+init()
