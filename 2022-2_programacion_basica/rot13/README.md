e9a666dec085fc394b705116d51326877d8c16c2
Aiman <aimanizales@gmail.com>
2022-10-12 01:20:28 -0500

    Added Eval dist 2

---
+++ 2022-2_programacion_basica/rot13/README.md
@@ -0,0 +1,13 @@
+# Programación básica
+
+## Eval. Distancia 2
+
+### Actividad 1:
+Ejecutar el archivo `rott13.py` con Python 3:
+```
+$ python3 rott13.py
+```
+---
+
+### Resumen del ejercicio:
+algoritmo de cifrado Cesar y/o ROT13
---
+++ 2022-2_programacion_basica/rot13/rot13.py
@@ -0,0 +1,35 @@
+# algoritmo de cifrado Cesar y/o ROT13
+
+alfabet = 'ABCDEFGHIJKLMNNÑOPQRSTUVWXYZ1234567890 '
+alfabetLength = len(alfabet)
+indexFactor = 5
+
+def init():
+    stringToEncript = input('\n>>>>> Escriba una palabra a transformar: ').upper()
+
+    print(f'1. Original:\t{stringToEncript}')
+
+    encriptedString = ''
+
+    iterator = 0
+    while iterator < len(stringToEncript):
+        currentLetter = stringToEncript[iterator]
+        letterPosInAlfabet = alfabet.find(currentLetter)
+
+        newIndexAlfabet = letterPosInAlfabet + indexFactor
+
+        if newIndexAlfabet > alfabetLength - 1:
+            newIndexAlfabet = newIndexAlfabet - alfabetLength
+        # print(f'{str(letterPosInAlfabet)}:{newIndexAlfabet}')
+
+        newLetter = alfabet[newIndexAlfabet]
+
+        encriptedString += newLetter
+        # print(currentLetter + ':' + newLetter)
+
+        iterator += 1
+
+    print(f'2. Encriptada:\t{encriptedString}')
+    init()
+
+init()
