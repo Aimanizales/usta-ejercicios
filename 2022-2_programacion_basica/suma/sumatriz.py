0c8ce6250e89f8fd97a48d82b4a852d8dc67fab4
Aiman <aimanizales@gmail.com>
2022-10-28 20:41:47 -0500

    Suma de rows de matrices cuadradas n x n y su media

--- 2022-2_programacion_basica/suma/sumatriz.py
+++ 2022-2_programacion_basica/suma/sumatriz.py
@@ -3,8 +3,45 @@
 # Cada posición de la matriz se genera 
 # como numero aleatorio entre 1 hasta 10.
 
+import numpy as np
+
+def createSquareMatrix():
+    global A
+    global matrixSize
+
+    matrixSize = np.random.randint(2,7)
+    A = np.random.randint(1,10, size = (matrixSize, matrixSize))
+    print(f"Matriz A de {matrixSize} x {matrixSize}:\n{A}")
 
 def init():
-    print('Hola')
+    createSquareMatrix()
+    sumarFilasMetodo1()
+    sumarFilasMetodo2()
+
+def sumarFilasMetodo1():
+    B = np.zeros((matrixSize, 2), dtype = float)
+    # print(range(len(A)))
+    
+    for nRow in range(len(A)):
+        row = A[nRow]
+        # print(f'row {nRow} = {row}')
+        # print(f'nRow {nRow} = {A[nRow, 0]}')
+        for n in range(len(A)):
+            B[nRow, 0] += A[nRow, n]
+            B[nRow, 1] = round(B[nRow, 0] / 3, 2)
+            # print(A[nRow, n])
+    
+    print(
+        '\nResultado con método 1 usando for()\n[suma, media]:\n',
+        B
+    )
+
+def sumarFilasMetodo2():
+    print('\nResultado con método 2 usando método iterable sum():')
+    
+    for nRow in range(len(A)):
+        sumRow = sum(A[nRow])
+        mediaRow = round(sumRow / len(A), 2)
+        print(f'suma = {sumRow} | media = {mediaRow}')
 
 init()
